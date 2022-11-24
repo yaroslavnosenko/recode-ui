d5ab51321ab05e60e53b21b829722ee5501c5644
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2022-11-24 22:29:55 +0100

    button h

--- components/menu/order-button.tsx
+++ components/menu/order-button.tsx
@@ -3,7 +3,7 @@ import { FiBell } from 'react-icons/fi'
 export const OrderButton = () => {
   return (
     <div className="max-w-2xl mx-auto p-6 pt-2">
-      <button className="bg-brand-600 w-full flex-1 flex items-center justify-center rounded-md py-2 active:opacity-50 font-medium">
+      <button className="bg-brand-600 w-full flex-1 flex items-center justify-center rounded-md py-3 active:opacity-50 font-medium">
         <FiBell strokeWidth={3} />
         <p className="ml-2">Make order</p>
       </button>

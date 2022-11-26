d83803b81ab11be1550680a8b0e794bdf96b9709
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2022-11-26 12:54:41 +0100

    added meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
         <body className="bg-light-100 dark:bg-dark-900 text-black dark:text-white">
           <Main />
           <NextScript />

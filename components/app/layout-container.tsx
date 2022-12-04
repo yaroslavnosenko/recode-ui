5aba57f37b5d411a139023f4331413c18389a0dc
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2022-12-04 20:13:48 +0100

    settings page

--- components/app/layout-container.tsx
+++ components/app/layout-container.tsx
@@ -3,5 +3,5 @@ import { ChakraComponent, Container, ContainerProps } from '@chakra-ui/react'
 export const LayoutContainer: ChakraComponent<'div'> = (
   props: ContainerProps
 ) => {
-  return <Container px={{ base: 6, lg: 8 }} maxWidth="full" {...props} />
+  return <Container px={{ base: 6, lg: 12 }} maxWidth="full" {...props} />
 }
--- components/app/pages/settings.tsx
+++ components/app/pages/settings.tsx
@@ -1,5 +1,18 @@
-import { Layout } from 'components/app'
+import { Heading, Text } from '@chakra-ui/react'
+
+import { EditMerchantForm, Layout, LayoutContainer } from 'components/app'
 
 export const Settings = () => {
-  return <Layout>Settings</Layout>
+  return (
+    <Layout>
+      <LayoutContainer py="12">
+        <Heading>Settings</Heading>
+        <Text opacity="0.5" mb="8">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+          repudiandae hic quas similique rem dolorem.
+        </Text>
+        <EditMerchantForm />
+      </LayoutContainer>
+    </Layout>
+  )
 }
